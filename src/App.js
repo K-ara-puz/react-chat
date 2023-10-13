eee92a1bb717c848594d0ea92197070c029a82aa
K-ara-puz <mis.viktoriya@hotmail.com>
2023-10-13 12:53:20 +0300

    added basic project schema

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import "./App.css";
 import Header from "./components/header/Header";
-import Navbar from "./components/navbar/Navbar";
 import Main from "./components/main/Main";
 import FriendsPanel from "./components/friendsPanel/FriendsPanel";
 
@@ -10,7 +9,6 @@ function App() {
       <Header></Header>
       <div className="app__wrapper">
         <div className="app__wrapper__content">
-          <Navbar></Navbar>
           <Main></Main>
         </div>
         <div className="app__wrapper__friends-bar">
